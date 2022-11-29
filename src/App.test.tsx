7d8d7b58efaa1c9542d4173124213b08e6244e51
Gordon Martin <gordon_martin@sil.org>
2022-11-29 15:17:21 -0800

    create separate list test file

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -6,13 +6,3 @@ test('renders react logo', () => {
     const linkElement = screen.getByAltText(/logo/i);
     expect(linkElement).toBeInTheDocument();
 });
-
-test('renders list of animals', () => {
-    render(<App />);
-    const dogItem = screen.getByText(/dog/i);
-    expect(dogItem).toBeVisible();
-    const catItem = screen.getByText(/cat/i);
-    expect(catItem).toBeVisible();
-    const birdItem = screen.getByText(/parakeet/i);
-    expect(birdItem).toBeVisible();
-});
