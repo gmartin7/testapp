0d3d08a2d86bfacf4f617e2e7ef44f86614425a7
Gordon Martin <gordon_martin@sil.org>
2022-11-29 15:09:44 -0800

    use styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,35 @@
 import { FunctionComponent } from 'react';
 import logo from './logo.svg';
-import './App.css';
 import List from "./components/list";
+import styled from "styled-components";
 
 export const App: FunctionComponent = () => (
-    <div className="App">
-        <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-        </header>
+    <AppWrapper>
+        <AppHeader>
+            <AppLogo src={logo} alt="logo" />
+        </AppHeader>
         <List />
-    </div>
+    </AppWrapper>
 );
+
+const AppWrapper = styled.div`
+    text-align: center;    
+`;
+
+const AppHeader = styled.div`
+    background-color: #282c34;
+    min-height: 40vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    font-size: calc(10px + 2vmin);
+    color: white;    
+`;
+
+const AppLogo = styled.img`
+    height: 20vmin;
+    pointer-events: none;
+`;
 
 export default App;
--- src/stories/Form.tsx
+++ src/stories/Form.tsx
@@ -1,6 +1,6 @@
-import React, { FunctionComponent } from "react";
+import { FunctionComponent, useState } from "react";
 import { Button } from "./Button";
-import "./form.css";
+import styled from "styled-components";
 
 interface FormProps {
     placeholder?: string;
@@ -12,41 +12,51 @@ export const Form: FunctionComponent<FormProps> = ({
     placeholder = "Enter your email",
     ...props
 }: FormProps) => {
-    const [details, setDetails] = React.useState(false);
+    const [details, setDetails] = useState(false);
 
     const onClick = () => {
         setDetails(true);
     };
 
     return (
-        <div className="form-wrapper">
-        <input
-            autoComplete="off"
-            placeholder="Enter your email address"
-            className={"storybook-input"}
-            name="email"
-            aria-label="email"
-            id="email"
-            data-testid="email"
-            {...props}
-        />
-        <input
-            autoComplete="off"
-            placeholder="Enter your password"
-            className={"storybook-input"}
-            name="password"
-            id="password"
-            data-testid="password"
-            {...props}
-        />
-        <Button label="Submit" onClick={() => onClick()} />
-        {details === true ? (
-            <p>
-            Get ready to experience great development experience with storybook
-            </p>
-        ) : (
-            ""
-        )}
-        </div>
+        <FormWrapper>
+            <input
+                autoComplete="off"
+                placeholder="Enter your email address"
+                className={"storybook-input"}
+                name="email"
+                aria-label="email"
+                id="email"
+                data-testid="email"
+                {...props}
+            />
+            <input
+                autoComplete="off"
+                placeholder="Enter your password"
+                className={"storybook-input"}
+                name="password"
+                id="password"
+                data-testid="password"
+                {...props}
+            />
+            <Button label="Submit" onClick={() => onClick()} />
+            {details === true ? (
+                <p>
+                Get ready to experience great development experience with storybook
+                </p>
+            ) : (
+                ""
+            )}
+        </FormWrapper>
     );
 };
+
+const FormWrapper = styled.div`
+    padding: 5rem;
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    width: 30%;
+`;
+
+export default Form;
